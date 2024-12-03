a289cf04e5efee88f67a7986d00c2228595c251a
Ravikant Singh <122196249+raavikant27@users.noreply.github.com>
2024-12-03 23:53:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-// complete the given function
+function isPalindrome(s) {
+    // Convert the string to lowercase and remove all non-alphanumeric characters
+    let cleanedString = s.toLowerCase().replace(/[^a-z0-9]/g, '');
 
-function palindrome(str){
+    // Compare the cleaned string with its reverse
+    let reversedString = cleanedString.split('').reverse().join('');
 
+    // Return true if the cleaned string is equal to the reversed string, otherwise return false
+    return cleanedString === reversedString;
 }
-module.exports = palindrome
+
+// Example usage
+console.log(isPalindrome("race a car"));  // Output: false
+console.log(isPalindrome("A man, a plan, a canal: Panama"));  // Output: true
